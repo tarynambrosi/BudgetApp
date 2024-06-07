d0522c253e96807a1bd5eb3b04b7e1ff8db4dbcd
tarynambrosi <tarynambrosi@gmail.com>
2024-06-07 10:51:43 -0500

    Add project files.

---
+++ BudgetApp.UI.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BudgetApp.UI.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
